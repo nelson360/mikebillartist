e8ebc2e9b7c5a8d4eeb397379254edaf7a8ec886
nelson360 <nelsonomondi99@gmail.com>
2022-12-04 10:20:51 +0300

    cont'd

--- src/components/ScheduleComponent.vue
+++ src/components/ScheduleComponent.vue
@@ -1,27 +1,117 @@
 <template>
-<p>
-    schedule
-</p>
-<table>
-    <tbody>
-        <thead></thead>
-        <tr>
-            <th>date</th>
-            <th>event</th>
-            <th>place</th>
-        </tr>
-        <tr>
-            <td>1111</td>
-            <td>black & white</td>
-            <td>yttt</td>
-        </tr>
-    </tbody>
-</table>
+    <br><br><br>
+
+    <div class="schedule">
+        <div class="caption">caption</div>
+        <div class="tableheader">header</div>
+        <div class="header-cell">Date</div>
+        <div class="header-cell">event</div>
+        <div class="header-cell">location</div>
+        <div class="header-cell">Venue</div>
+        <div class="table-body">
+            <div class="table-row">
+    
+                <div class="body-cell">cell 1-1</div>
+                <div class="body-cell">cell 1-2</div>
+                <div class="body-cell"> 1-1</div>
+                <div class="body-cell">cell 1-3</div>
+            </div>
+            <div class="table-row">
+                <div class="body-cell">cell 1-1</div>
+                <div class="body-cell">cell 1-2</div>
+                <div class="body-cell"> 1-1</div>
+                <div class="body-cell">cell 1-3</div>
+            </div>
+            <div class="table-row">
+                <div class="body-cell">cell 1-1</div>
+                <div class="body-cell">cell 1-2</div>
+                <div class="body-cell"> 1-1</div>
+                <div class="body-cell">cell 1-3</div>
+            </div>
+            <div class="table-row">
+                <div class="body-cell">cell 1-1</div>
+                <div class="body-cell">cell 1-2</div>
+                <div class="body-cell"> 1-1</div>
+                <div class="body-cell">cell 1-3</div>
+            </div>
+            <div class="table-row">
+                <div class="body-cell">cell 1-1</div>
+                <div class="body-cell">cell 1-2</div>
+                <div class="body-cell"> 1-1</div>
+                <div class="body-cell">cell 1-3</div>
+            </div>
+            <div class="table-row">
+                <div class="body-cell">cell 1-1</div>
+                <div class="body-cell">cell 1-2</div>
+                <div class="body-cell"> 1-1</div>
+                <div class="body-cell">cell 1-3</div>
+            </div>
+    
+        </div>
+    
+        <div class="table-footer">footer tings
+            <div class="footer-cell">Footer1</div>
+            <div class="footer-cell">Footer2</div>
+            <div class="footer-cell">Footer3</div>
+        </div>
+    
+    </div>
+    <br><br>
 </template>
 
 <style>
-table {
+.schedule {
     display: table;
-    font-size: large;
+    height: 100%;
+    width: 100%;
+
+}
+
+.caption {
+    display: table-caption;
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.tableheader {
+    display: table-header-group;
+    background-color: gray;
+    font-weight: bold;
+    font-size: 25px;
+}
+
+.header-cell {
+    display: table-cell;
+    padding: 10px;
+    text-align: justify;
+    border-bottom: 1px solid black;
+}
+
+.table-body {
+    display: table-row-group;
+}
+
+.table-row {
+    display: table-row;
+}
+
+.body-cell {
+    display: table-cell;
+}
+
+.table-footer {
+    display: table-footer-group;
+    background-color: gray;
+    font-weight: bold;
+    font-size: 25px;
+    color: rgba(255, 255, 255, 0.45);
+}
+
+.footer-cell {
+    display: table-cell;
+    padding: 10px;
+    text-align: justify;
+    border-bottom: 1px solid black;
 }
 </style>
